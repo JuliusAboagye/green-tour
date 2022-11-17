c972ddc0ac65a0dbb03bd2e3b5b4b7d45b8c9aa5
Julius <juliusaboagye45@gmail.com>
2022-11-17 10:30:00 +0000

    handled missing route

--- app.js
+++ app.js
@@ -8,4 +8,11 @@ app.use(express.json());
 
 app.use('/api/v1/tours', tourRoute);
 
+app.all('*', (req, res, next) => {
+  res
+    .status(404)
+    .json({ status: 'fail', message: `Can't find ${req.originalUrl}` });
+  next();
+});
+
 module.exports = app;
