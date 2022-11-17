49f94baed18f95e3c279a6e0f2cec2d2d0aaad2b
Julius <juliusaboagye45@gmail.com>
2022-11-17 12:45:21 +0000

    handled errors for dev and prod

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,30 @@
+const sendErrorDev = (err, res) => {
+  res.status(error.statusCode).json({
+    status: error.status,
+    error: error,
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+const sendErrorProd = (error, res) => {
+  if (err.isOperational) {
+    res
+      .status(error.statusCode)
+      .json({ status: error.status, message: error.message });
+  } else {
+    console.error('Error 💥', error);
+    res.status(500).json({ status: 'error', message: 'Something went wrong' });
+  }
+};
+
 module.exports = (error, req, res, next) => {
   error.statusCode = error.statusCode || 500;
   error.message = error.message || 'error';
 
-  res
-    .status(error.statusCode)
-    .json({ status: error.status, message: error.message });
+  if (process.env.NODE_ENV == 'development') {
+    sendErrorDev(error, res);
+  } else if (process.env.NODE_ENV == 'production') {
+    sendErrorProd(error, res);
+  }
 };
