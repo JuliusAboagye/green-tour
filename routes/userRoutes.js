0578372450a0ef5afe54ac6ad7c4f529657eae85
Julius <juliusaboagye45@gmail.com>
2022-11-18 18:39:52 +0000

    get all users route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const expess = require('express');
 const router = expess.Router();
 const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
 
 router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+
+router.route('/').get(userController.getAllUsers);
 
 module.exports = router;
