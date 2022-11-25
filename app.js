168317256dde3436a41920ce45f450786d825111
Julius <juliusaboagye45@gmail.com>
2022-11-25 00:48:05 +0000

    added rate limiting and secure headers

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-const fs = require('fs');
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const app = express();
 const morgan = require('morgan');
 const tourRoute = require('./routes/tourRoutes');
 const userRoute = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-app.use(morgan('dev'));
-app.use(express.json());
 
+const limiter = rateLimit({
+  max: 1000,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP,please try again in an hour!',
+});
+
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json({ limit: '10kb' }));
+app.use('/api', limiter);
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/tours', tourRoute);
 
